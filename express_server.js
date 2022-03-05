819f06d36261a6e8cee6f8aade089e01c235a210
Brenda <brendamvreal@gmail.com>
2022-03-05 01:03:17 -0500

    Set up a GET request to redirect any request to /u/:shortURL to its longURL

--- express_server.js
+++ express_server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
@@ -9,22 +12,13 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-const bodyParser = require("body-parser");
-
 function generateRandomString() {
   let result = Array.from(Array(6), () => Math.floor(Math.random() * 36).toString(36)).join("");
   return result;
 }
 
-app.use(bodyParser.urlencoded({extended: true}));
-
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
-
-app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
-  res.render("urls_show", templateVars);
+app.get("/", (req, res) => {
+  res.send("Hello!");
 });
 
 app.get("/urls", (req, res) => {
@@ -32,12 +26,29 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+app.post("/urls", (req, res) => {
+  const randomURL = generateRandomString();
+  urlDatabase[randomURL] = req.body.longURL;
+  res.redirect(`/urls/${randomURL}`);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  res.render("urls_show", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = urlDatabase[shortURL]
+  if (urlDatabase[shortURL]) {
+    res.redirect(longURL);
+  } else { 
+ res.send("Invalid input!")
+  }
 });
 
 app.get("/urls.json", (req, res) => {
@@ -47,15 +58,12 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
-//  whatever the newly formed string is, make it equal to req.body.longURL (which represents the url that you used in browser)
-// create a variable that stores the function, that variable is equal to req.body.longURL
-//res.redirect would be req.body.longURL
-//  the generated string is a key => urlDatabase.b2xVn2(function)
 
-app.post("/urls", (req, res) => {
-  const randomURL = generateRandomString();
-  urlDatabase[randomURL] = req.body.longURL;
-  res.redirect(`/urls/${randomURL}`);
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
 });
+
+
+
 
 
