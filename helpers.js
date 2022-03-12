d82a12c56e5f747da915c4f78b31a88e83ea896c
Brenda <brendamvreal@gmail.com>
2022-03-11 23:22:45 -0500

    Create a helper functions module and exported the helper functions to that file

---
+++ helpers.js
@@ -0,0 +1,63 @@
+const bcrypt = require('bcryptjs');
+
+const generateRandomString = function() { 
+  let result = Array.from(Array(6), () => Math.floor(Math.random() * 36).toString(36)).join("");
+  return result;
+};
+
+const findUserByEmail = function(email, users) {  // helper function used in authenticateUser function
+  for (const userID in users) {
+    if (users[userID].email === email) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const findUserByPassword = function(password, users) {  // helper function used in authenticateUser function
+  for (const userID in users) {
+    if (bcrypt.compareSync(password, users[userID].password)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const authenticateUser = function(email, password, users) {  // helper function used in POST /login
+  for (const user in users) {
+    if (users[user].email === email && bcrypt.compareSync(password, users[user].password)) {
+      return users[user]; // this is for the login 
+    }
+  }
+  const userFound = findUserByEmail(email, users);
+  const passwordFound = findUserByPassword(password, users);
+  if (!userFound && !passwordFound) {
+    return false;
+  }
+  if (userFound || passwordFound) {
+    return true;
+  }
+  return true;
+};
+
+const urlsForUser = function(id, uDb) { 
+  newObj = {};
+  for (const k in uDb) { 
+    if (id === uDb[k].userID) { 
+      newObj[k] = {             
+        longURL: uDb[k].longURL,
+        userID: uDb[k].userID
+      };
+    }
+  } 
+  return newObj;
+};
+
+const findUserByShortURL = function(shortURL, id, uDb) { 
+  if (uDb[shortURL].userID === id) {
+    return true;
+  }
+  return false;       
+};
+
+module.exports = { generateRandomString, authenticateUser, urlsForUser, findUserByShortURL };
